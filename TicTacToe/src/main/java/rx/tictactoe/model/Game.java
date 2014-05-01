3dcf36a071f87c0a73457f1275eec76dbb3ab8ef
rvanheest <A.w.m.vanHeest@student.tudelft.nl>
2014-05-01 18:51:45 +0200

    Added controlling methods to Game and made other methods for GameState

--- TicTacToe/src/main/java/rx/tictactoe/model/Game.java
+++ TicTacToe/src/main/java/rx/tictactoe/model/Game.java
@@ -1,5 +1,7 @@
 package rx.tictactoe.model;
 
+import java.util.Random;
+
 import rx.Observable;
 import rx.subjects.PublishSubject;
 
@@ -8,48 +10,74 @@ public class Game extends Observable<GameState> implements IGameStateManager {
 	private final Board board;
 	private GameState gameState;
 	private Sprite turn;
-	private PublishSubject<GameState> subject;
+	private final PublishSubject<GameState> subject;
 
-	public Game(Board board, Sprite first) {
-		this(board, first, PublishSubject.create());
+	public Game(Board board) {
+		this(board, PublishSubject.create());
+		this.startNewGame();
 	}
 	
-	private Game(Board board, Sprite first, PublishSubject<GameState> subject) {
+	private Game(Board board, PublishSubject<GameState> subject) {
 		super(subscriber -> subject.subscribe(subscriber));
 		this.board = board;
 
-		this.gameState = GameState.STOPPED;
-		this.turn = first;
+		this.turn = this.determineFirst();
+		this.gameState = this.turn == Sprite.X ? GameState.TURN_X : GameState.TURN_O;
 		this.subject = subject;
+	}
+	
+	private Sprite determineFirst() {
+		return new Random().nextBoolean() ? Sprite.X : Sprite.O;
 	}
 	
 	public Board getBoard() {
 		return this.board;
 	}
+	
+	public GameState getGameState() {
+		return this.gameState;
+	}
 
 	public void performMove(int x, int y) {
-		if (this.gameState == GameState.STARTED) {
+		if (this.gameState == GameState.TURN_X
+				|| this.gameState == GameState.TURN_O) {
 			this.board.set(this.turn, x, y);
+		}
+		
+		if (this.gameState == GameState.TURN_X
+				|| this.gameState == GameState.TURN_O) {
 			this.switchTurns();
 		}
 	}
 
 	private void switchTurns() {
-		if (this.gameState == GameState.STARTED) {
-    		if (this.turn == Sprite.X) {
-    			this.turn = Sprite.O;
-    		}
-    		else {
-    			assert this.turn == Sprite.O;
-    			this.turn = Sprite.X;
-    		}
+		if (this.turn == Sprite.X) {
+			this.turn = Sprite.O;
+			this.gameState = GameState.TURN_O;
 		}
+		else {
+			assert this.turn == Sprite.O;
+			this.turn = Sprite.X;
+			this.gameState = GameState.TURN_X;
+		}
+		this.subject.onNext(this.gameState);
 	}
 
 	@Override
 	public void startNewGame() {
 		this.board.reset();
-		this.gameState = GameState.STARTED;
+		switch (this.gameState) {
+			case WON_O:
+				this.gameState = GameState.TURN_X;
+				this.turn = Sprite.X;
+				break;
+			case WON_X:
+				this.gameState = GameState.TURN_O;
+				this.turn = Sprite.O;
+				break;
+			default:
+				break;
+		}
 		this.subject.onNext(this.gameState);
 	}
 
