93760735ff0b944b20e07809b7ff07fe65d8c4f3
rvanheest <A.w.m.vanHeest@student.tudelft.nl>
2014-05-08 13:31:32 +0200

    Pipe technology improved. Now moving!

--- FlappyBird/src/main/java/rx/flappyBirds/Main.java
+++ FlappyBird/src/main/java/rx/flappyBirds/Main.java
@@ -2,6 +2,7 @@ package rx.flappyBirds;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
@@ -17,7 +18,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		int screenWidth = 691;
+		int screenWidth = 691; // 691
 		int screenHeight = 921;
 
 		StackPane root = new StackPane();
@@ -25,7 +26,7 @@ public class Main extends Application {
 		Scene scene = new Scene(root, screenWidth, screenHeight);
 
 		// time
-		Observable<Integer> clock = Observable.timer(0, 16, TimeUnit.MILLISECONDS)
+		Observable<Integer> clock = Observable.timer(0, 1, TimeUnit.MILLISECONDS)
 				.map(x -> 1)
 				.observeOn(new FxScheduler());
 
@@ -47,28 +48,54 @@ public class Main extends Application {
 			tile.setTranslateX(i * bottomWidth);
 			bottom.add(tile);
 		}
-		
-		clock.map(i -> 1).subscribe(v -> {
-			bottom.stream().forEach(tile -> {
-				double dx = tile.getTranslateX();
-				if (dx <= -bottomWidth) {
-					tile.setTranslateX(screenWidth - v);
-				}
-				else {
-					tile.setTranslateX(dx - v);
-				}
-			});
-		});
+
+		clock.map(i -> 1).subscribe(v ->
+				bottom.stream().forEach(tile -> {
+					double dx = tile.getTranslateX();
+					if (dx <= -bottomWidth) {
+						tile.setTranslateX(screenWidth - v);
+					}
+						else {
+							tile.setTranslateX(dx - v);
+						}
+					}));
 
 		// Pipe
 		Image pipeImg = new Image("pipe.png");
-		ImageView pipeUp = new ImageView(pipeImg);
-		ImageView pipeDown = new ImageView(pipeImg);
-		root.getChildren().add(pipeUp);
-		root.getChildren().add(pipeDown);
-		pipeUp.setRotate(180.0);
-		pipeUp.setTranslateY(200 - screenHeight);// change this!
-		pipeDown.setTranslateY(200);// change this!
+		double pipeWidth = pipeImg.getWidth();
+		double pipeDist = screenWidth * 2 / 3;
+		List<ImageView[]> pipes = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			ImageView pipeUp = new ImageView(pipeImg);
+			ImageView pipeDown = new ImageView(pipeImg);
+			root.getChildren().add(pipeUp);
+			root.getChildren().add(pipeDown);
+			pipeUp.setRotate(180.0);
+			pipeUp.setTranslateX(i * pipeDist);
+			pipeDown.setTranslateX(i * pipeDist);
+			pipeUp.setTranslateY(200 - screenHeight); // change this!
+			pipeDown.setTranslateY(200); // change this!
+			pipes.add(new ImageView[] { pipeUp, pipeDown });
+		}
+
+		Random random = new Random();
+		clock.map(i -> 1).subscribe(v ->
+				pipes.stream().forEach(array -> {
+					ImageView up = array[0];
+					ImageView down = array[1];
+					double dx = up.getTranslateX();
+					if (dx <= -pipeWidth) {
+						double d = random.nextDouble();
+						up.setTranslateX(3 * pipeDist - pipeWidth);
+						down.setTranslateX(3 * pipeDist - pipeWidth);
+						up.setTranslateY(d * 600 - screenHeight); // change this!
+						down.setTranslateY(d * 600); // change this!
+					}
+						else {
+							up.setTranslateX(dx - v);
+							down.setTranslateX(dx - v);
+						}
+					}));
 
 		stage.setTitle("Flappy Bird");
 		stage.setScene(scene);
