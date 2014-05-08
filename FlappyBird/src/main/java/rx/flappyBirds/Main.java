99d6bacf54ede5812c8312c125aa691b27608126
rvanheest <A.w.m.vanHeest@student.tudelft.nl>
2014-05-08 21:29:08 +0200

    Finished pipe technology

--- FlappyBird/src/main/java/rx/flappyBirds/Main.java
+++ FlappyBird/src/main/java/rx/flappyBirds/Main.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import rx.Observable;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -13,12 +12,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import rx.Observable;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		int screenWidth = 691; // 691
+		int screenWidth = 691;
 		int screenHeight = 921;
 
 		StackPane root = new StackPane();
@@ -26,7 +26,7 @@ public class Main extends Application {
 		Scene scene = new Scene(root, screenWidth, screenHeight);
 
 		// time
-		Observable<Integer> clock = Observable.timer(0, 1, TimeUnit.MILLISECONDS)
+		Observable<Integer> clock = Observable.timer(0, 16, TimeUnit.MILLISECONDS)
 				.map(x -> 1)
 				.observeOn(new FxScheduler());
 
@@ -51,45 +51,47 @@ public class Main extends Application {
 
 		clock.map(i -> 1).subscribe(v ->
 				bottom.stream().forEach(tile -> {
-					double dx = tile.getTranslateX();
-					if (dx <= -bottomWidth) {
+					double translateX = tile.getTranslateX();
+					if (translateX <= -bottomWidth) {
 						tile.setTranslateX(screenWidth - v);
 					}
 						else {
-							tile.setTranslateX(dx - v);
+							tile.setTranslateX(translateX - v);
 						}
 					}));
 
-		// Pipe
+		// Pipes
 		Image pipeImg = new Image("pipe.png");
 		double pipeWidth = pipeImg.getWidth();
 		double pipeDist = screenWidth * 2 / 3;
 		List<ImageView[]> pipes = new ArrayList<>();
+		int[] heights = { 100, 150, 200, 250, 300, 350, 400, 450, 500 };
+		Random random = new Random();
 		for (int i = 0; i < 3; i++) {
 			ImageView pipeUp = new ImageView(pipeImg);
 			ImageView pipeDown = new ImageView(pipeImg);
 			root.getChildren().add(pipeUp);
 			root.getChildren().add(pipeDown);
 			pipeUp.setRotate(180.0);
-			pipeUp.setTranslateX(i * pipeDist);
-			pipeDown.setTranslateX(i * pipeDist);
-			pipeUp.setTranslateY(200 - screenHeight); // change this!
-			pipeDown.setTranslateY(200); // change this!
+			pipeUp.setTranslateX((i + 2) * pipeDist);
+			pipeDown.setTranslateX((i + 2) * pipeDist);
+			int h = heights[random.nextInt(heights.length)];
+			pipeUp.setTranslateY(h - screenHeight);
+			pipeDown.setTranslateY(h - screenHeight / 8);
 			pipes.add(new ImageView[] { pipeUp, pipeDown });
 		}
 
-		Random random = new Random();
 		clock.map(i -> 1).subscribe(v ->
 				pipes.stream().forEach(array -> {
 					ImageView up = array[0];
 					ImageView down = array[1];
 					double dx = up.getTranslateX();
 					if (dx <= -pipeWidth) {
-						double d = random.nextDouble();
 						up.setTranslateX(3 * pipeDist - pipeWidth);
 						down.setTranslateX(3 * pipeDist - pipeWidth);
-						up.setTranslateY(d * 600 - screenHeight); // change this!
-						down.setTranslateY(d * 600); // change this!
+						int h = heights[random.nextInt(heights.length)];
+						up.setTranslateY(h - screenHeight);
+						down.setTranslateY(h - screenHeight / 8);
 					}
 						else {
 							up.setTranslateX(dx - v);
