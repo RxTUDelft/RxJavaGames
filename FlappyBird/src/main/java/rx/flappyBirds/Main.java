e3e29972f94fc0f34af845409a2d7df8d3eb0333
rvanheest <A.w.m.vanHeest@student.tudelft.nl>
2014-05-08 09:51:53 +0200

    Added moving floor and scheduler for Platform thread

--- FlappyBird/src/main/java/rx/flappyBirds/Main.java
+++ FlappyBird/src/main/java/rx/flappyBirds/Main.java
@@ -1,5 +1,10 @@
 package rx.flappyBirds;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -14,11 +19,16 @@ public class Main extends Application {
 	public void start(Stage stage) {
 		int screenWidth = 691;
 		int screenHeight = 921;
-		
+
 		StackPane root = new StackPane();
 		root.setAlignment(Pos.BOTTOM_LEFT);
 		Scene scene = new Scene(root, screenWidth, screenHeight);
-		
+
+		// time
+		Observable<Integer> clock = Observable.timer(0, 16, TimeUnit.MILLISECONDS)
+				.map(x -> 1)
+				.observeOn(new FxScheduler());
+
 		// Background
 		Image bgImg = new Image("background.png");
 		ImageView background = new ImageView(bgImg);
@@ -28,14 +38,28 @@ public class Main extends Application {
 		// Bottom
 		Image bottomImg = new Image("ground.png");
 		double bottomWidth = bottomImg.getWidth();
-		int numberOfImgs = Double.valueOf(Math.ceil(screenWidth / bottomWidth)).intValue();
+		int numberOfImgs = Double.valueOf(Math.ceil(screenWidth / bottomWidth)).intValue() + 1;
 
+		List<ImageView> bottom = new ArrayList<>();
 		for (int i = 0; i < numberOfImgs; i++) {
 			ImageView tile = new ImageView(bottomImg);
 			root.getChildren().add(tile);
 			tile.setTranslateX(i * bottomWidth);
+			bottom.add(tile);
 		}
 		
+		clock.map(i -> 1).subscribe(v -> {
+			bottom.stream().forEach(tile -> {
+				double dx = tile.getTranslateX();
+				if (dx <= -bottomWidth) {
+					tile.setTranslateX(screenWidth - v);
+				}
+				else {
+					tile.setTranslateX(dx - v);
+				}
+			});
+		});
+
 		// Pipe
 		Image pipeImg = new Image("pipe.png");
 		ImageView pipeUp = new ImageView(pipeImg);
@@ -43,12 +67,9 @@ public class Main extends Application {
 		root.getChildren().add(pipeUp);
 		root.getChildren().add(pipeDown);
 		pipeUp.setRotate(180.0);
-		pipeUp.setTranslateY(200 - screenHeight);//change this!
-		pipeDown.setTranslateY(200);//change this!
-		
-		
-		
-		
+		pipeUp.setTranslateY(200 - screenHeight);// change this!
+		pipeDown.setTranslateY(200);// change this!
+
 		stage.setTitle("Flappy Bird");
 		stage.setScene(scene);
 		stage.show();
